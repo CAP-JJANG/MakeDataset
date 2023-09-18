cb7e59aa66c6d9580443fd0e644876569f2a8d43
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-09-18 17:08:27 +0900

    폴더 선택 가능하게 코드 추가

--- toWav.py
+++ toWav.py
@@ -3,8 +3,6 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 from pydub import AudioSegment
 
-i = 0;
-
 def is_folderpath(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
@@ -18,13 +16,14 @@ for dirname, _, filenames in os.walk("dataset/m4a"):
         start_pos = m4a_file.rfind('/')
         alpha = m4a_file[start_pos-1:start_pos]
 
-        if (i != 0):
-            if (before_alpha != alpha):
-                i = 1
-            else:
-                i += 1
-        else:
-            i += 1
+        i = 1123
+        # if (i != 0):
+        #     if (before_alpha != alpha):
+        #         i = 1
+        #     else:
+        #         i += 1
+        # else:
+        #     i += 1
 
         before_alpha = alpha
 
